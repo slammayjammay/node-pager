35a1402561fad385f7e8f3cde4453c425ed0f13b
Scott McKenzie <slammayjammay@gmail.com.com>
2017-01-13 17:30:36 -0800

    Use promise instead

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ const PAGER_COMMAND_LOC = './helpers/pager'
  * @param {string} text - A string of text.
  * @return {null}
  */
-const pager = (text, exitCallback) => {
-	let pagerCommand = join(__dirname, PAGER_COMMAND_LOC)
-	spawnSync(pagerCommand, [text], { stdio: 'inherit' })
+const pager = (text) => {
+	return new Promise((resolve) => {
+		let pagerCommand = join(__dirname, PAGER_COMMAND_LOC)
+		spawnSync(pagerCommand, [text], { stdio: 'inherit' })
 
-	// I think this works? exitCallback seems to execute only after the less pager
-	// is exited.
-	setTimeout(exitCallback, 0)
+		// I think this works? resolve seems to execute only after the less pager is exited.
+		setTimeout(resolve, 0)
+	})
 }
 
 module.exports = pager
