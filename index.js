99cb604fcda505a53dc63b3bb2135b4dcdb0ad98
Scott McKenzie <slammayjammay@gmail.com.com>
2017-01-13 17:25:10 -0800

    Allow for pager exit callbacks (I think)

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ const PAGER_COMMAND_LOC = './helpers/pager'
  * @param {string} text - A string of text.
  * @return {null}
  */
-const pager = (text) => {
+const pager = (text, exitCallback) => {
 	let pagerCommand = join(__dirname, PAGER_COMMAND_LOC)
 	spawnSync(pagerCommand, [text], { stdio: 'inherit' })
+
+	// I think this works? exitCallback seems to execute only after the less pager
+	// is exited.
+	setTimeout(exitCallback, 0)
 }
 
 module.exports = pager
