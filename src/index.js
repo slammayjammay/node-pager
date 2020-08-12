fb7801066c2645c742e7e68180c8b62a37a262ea
Scott McKenzie <slammayjammay@gmail.com>
2020-08-11 20:38:50 -0700

    Randomize heredoc id

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 const { join } = require('path');
 const { spawnSync } = require('child_process');
 
+function createUniqueId() {
+	return Math.random().toFixed(20).slice(2);
+}
+
 module.exports = string => {
 	return new Promise(resolve => {
 		if (process.platform === 'win32') {
 			const file = join(__dirname, '../helpers/pager.ps1');
 			spawnSync('powershell', ['-File', file, string], { stdio: 'inherit' });
 		} else {
-			const command = `(\ncat << "EOF"\n${string}\nEOF\n) | less -R`;
+			const id = createUniqueId();
+			const command = `(\ncat << "${id}"\n${string}\n${id}\n) | less -R`;
 			spawnSync(command, [], { shell: true, stdio: 'inherit' });
 		}
 
