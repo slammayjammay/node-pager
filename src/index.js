7e61324feb1fba365293c1c9b11cd9804784b968
Scott McKenzie <slammayjammay@gmail.com>
2021-02-06 15:17:30 -0800

    Use tmp files for big strings

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
+const { writeSync } = require('fs');
 const { join } = require('path');
 const { spawnSync } = require('child_process');
+const tmp = require('tmp');
 
-function createUniqueId() {
-	return Math.random().toFixed(20).slice(2);
+function windows(string) {
+	const file = join(__dirname, '../helpers/pager.ps1');
+	spawnSync('powershell', ['-File', file, string], { stdio: 'inherit' });
 }
 
 module.exports = string => {
 	return new Promise(resolve => {
 		if (process.platform === 'win32') {
-			const file = join(__dirname, '../helpers/pager.ps1');
-			spawnSync('powershell', ['-File', file, string], { stdio: 'inherit' });
-		} else {
-			const id = createUniqueId();
-			const command = `(\ncat << "${id}"\n${string}\n${id}\n) | less -R`;
-			spawnSync(command, [], { shell: true, stdio: 'inherit' });
+			windows(string);
+			return resolve();
 		}
 
-		resolve();
+		tmp.file((err, path, fd, cleanupCb) => {
+			if (err) throw err;
+
+			writeSync(fd, string);
+			spawnSync(`less -R "${path}"`, { shell: true, stdio: 'inherit' });
+
+			cleanupCb();
+			resolve();
+		});
 	});
 };
